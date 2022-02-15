3660e66e2042554f1c8d1af22fe9ae85d88ca194
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-02-15 18:57:26 +0200

    homework was done

--- homework-1/app.js
+++ homework-1/app.js
@@ -5,13 +5,16 @@ const onlineUsers = [
   { name: "Andrii", age: 22, city: "Lviv" },
   { name: "Mariana", age: 18, city: "Lviv" },
 ];
-const inPersonUsers = [{ name: "Ann", age: 25, city: "Kyiv" }];
+const inPersonUsers = [
+  { name: "Ann", age: 25, city: "Kyiv" },
+  { name: "Valera", age: 35, city: "Odessa" },
+];
 
 for (const user of onlineUsers) {
-  for (const key in index) {
-    fs.appendFile(
+  for (const key in user) {
+    fs.writeFileSync(
       path.join(__dirname, "main", "online", "online.txt"),
-      `${key}:${user[key]}`,
+      `\n${key}:${user[key]};\n`,
       (err) => {
         if (err) {
           console.log(err);
@@ -21,3 +24,52 @@ for (const user of onlineUsers) {
     );
   }
 }
+
+for (const user of inPersonUsers) {
+  for (const key in user) {
+    fs.writeFileSync(
+      path.join(__dirname, "main", "inPerson", "inPerson.txt"),
+      `\n${key}:${user[key]};\n`,
+      (err) => {
+        if (err) {
+          console.log(err);
+          throw err;
+        }
+      }
+    );
+  }
+}
+
+function changeUsers() {
+  for (const user of onlineUsers) {
+    for (const key in user) {
+      fs.appendFileSync(
+        path.join(__dirname, "main", "inPerson", "inPerson.txt"),
+        `\n${key}:${user[key]};\n`,
+        (err) => {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+        }
+      );
+    }
+  }
+
+  for (const user of inPersonUsers) {
+    for (const key in user) {
+      fs.appendFileSync(
+        path.join(__dirname, "main", "online", "online.txt"),
+        `\n${key}:${user[key]};\n`,
+        (err) => {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+        }
+      );
+    }
+  }
+}
+
+changeUsers();
