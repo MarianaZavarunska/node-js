412581dd8cd7bf9a6704d05f203856afaa53fe78
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-02-16 10:10:51 +0200

    task 3 was done

--- classwork-1/app.js
+++ classwork-1/app.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const fs = require("fs");
+const { debuglog } = require("util");
 
-condole.log("====Task 1====");
+// condole.log("====Task 1====");
 
 // 1. Спробуйте створити якийсь файл txt, прочитайте з нього дані і одразу, дані які ви отримали запишіть їх в інший файл.
 
@@ -43,7 +44,7 @@ condole.log("====Task 1====");
 //   }
 // );
 
-condole.log("====Task 2====");
+// console.log("====Task 2====");
 
 // 2. Створіть файл ( можете вручну ) заповніть його якимись даними
 // Прочитайте його, скопіюйте всі дані з нього і перенесіть їх в нову папку та файл в ній, старий файл видаліть після того як все завершиться. Також вийде callback hell
@@ -103,7 +104,7 @@ condole.log("====Task 2====");
 //   }
 // });
 
-condole.log("====Task 3====");
+// condole.log("====Task 3====");
 
 // 3. Створіть папку (можете вручну) напишіть скріпт який створить в ній якись дані (можуть бути нові папки і файли(в файли запишіть якусь дату) )
 
@@ -155,57 +156,95 @@ condole.log("====Task 3====");
 // і напишіть функцію яка буде зчитувати папку і перевіряти якщо дані які в ній лежать - це файли тоді вам потрібно їх очистити, але не видаляти, якщо дані - це папки, вам потрібно їх перейменувати і додати до назви префікс _new
 
 function readData() {
-  fs.readdir(path.join(__dirname, "task-3"), (err, files1) => {
-    if (err) {
-      console.log(err);
-      throw err;
-    }
-    files1.forEach((file1) => {
-      console.log(file1);
-      if (file1.isFile()) {
-        fs.truncate(path.join(__dirname, "task-3", `${file1}`), 0, (err) => {
-          if (err) {
-            console.log(err);
-            throw err;
-          }
-          console.log("done1");
-        });
+  fs.readdir(
+    path.join(__dirname, "task-3"),
+    { withFileTypes: true },
+    (err, listOfFiles) => {
+      if (err) {
+        console.log(err);
+        throw err;
       }
-      fs.readdir(path.join(__dirname, "task-3", `${file1}`), (err, files2) => {
-        if (err) {
-          console.log(err);
-          throw err;
+      listOfFiles.forEach((file1) => {
+        console.log(file1.name);
+        if (file1.isFile()) {
+          fs.truncate(
+            path.join(__dirname, "task-3", `${file1.name}`),
+            0,
+            (err) => {
+              if (err) {
+                console.log(err);
+                throw err;
+              }
+              console.log("done1");
+            }
+          );
+        } else {
+          fs.rename(
+            path.join(__dirname, "task-3", `${file1.name}`),
+            path.join(__dirname, "task-3", `new_${file1.name}`),
+            (err) => {
+              if (err) {
+                console.log(err);
+                throw err;
+              }
+            }
+          );
+          fs.readdir(
+            path.join(__dirname, "task-3", `${file1.name}`),
+            { withFileTypes: true },
+            (err, listOfFiles2) => {
+              if (err) {
+                console.log(err);
+                throw err;
+              }
+              listOfFiles2.forEach((file2) => {
+                console.log(file2);
+                if (file2.isFile()) {
+                  fs.truncate(
+                    path.join(
+                      __dirname,
+                      "task-3",
+                      `${file1.name}`,
+                      `${file2.name}`
+                    ),
+                    0,
+                    (err) => {
+                      if (err) {
+                        console.log(err);
+                        throw err;
+                      }
+                      console.log("done2");
+                    }
+                  );
+                } else {
+                  fs.rename(
+                    path.join(
+                      __dirname,
+                      "task-3",
+                      `${file1.name}`,
+                      `${file2.name}`
+                    ),
+                    path.join(
+                      __dirname,
+                      "task-3",
+                      `${file1.name}`,
+                      `${file2.name}`
+                    ),
+                    (err) => {
+                      if (err) {
+                        console.log(err);
+                        throw err;
+                      }
+                    }
+                  );
+                }
+              });
+            }
+          );
         }
-        files2.forEach((file2) => {
-          console.log(file2);
-          if (file2.isFile()) {
-            fs.truncate(
-              path.join(__dirname, "task-3", `${file2}`, `${file2}`),
-              0,
-              (err) => {
-                if (err) {
-                  console.log(err);
-                  throw err;
-                }
-                console.log("done2");
-              }
-            );
-          } else {
-            fs.rename(
-              path.join(__dirname, "task-3", `${file2}`),
-              path.join(__dirname, "task-3", `new${file2}`),
-              (err) => {
-                if (err) {
-                  console.log(err);
-                  throw err;
-                }
-              }
-            );
-          }
-        });
       });
-    });
-  });
+    }
+  );
 }
 
 readData();
