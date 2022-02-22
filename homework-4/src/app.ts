66a2df664779f9e7dd57b0c40ec9c8b9a9a3d90e
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-02-22 18:14:23 +0200

    initial commit

--- homework-4/src/app.ts
+++ homework-4/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { arr } from './users';
 
 const app = express();
+console.log(arr);
 
-app.listen(5300,() => {
-    console.log("Server has started 🚀")
-})
+app.listen(5300, () => {
+    console.log('Server has started 🚀');
+});
